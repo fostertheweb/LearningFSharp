f77a60169804283f4cc76823d2c0103cdd5e2e77
Jonathan Foster <jxf9099@rit.edu>
2020-06-17 10:22:21 -0400

    Add project files.

---
+++ LearningFSharp/Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open FSharp.Data
+open Newtonsoft.Json
+
+type Sighting =
+  { EventDateTime: DateTime;
+    City: String;
+    State: String;
+    Shape: String;
+    Duration: String;
+    Summary: String;
+    PostedDate: DateTime; }
+
+let createSighting (tableRow: HtmlNode) =
+    let tableData = tableRow.CssSelect("td") |> List.map (fun td -> td.InnerText().Trim())
+    Console.Write "boop"
+
+[<EntryPoint>]
+let main argv =
+    let doc = HtmlDocument.Load("http://www.nuforc.org/webreports/ndxe202005.html")
+    let sightings = doc.CssSelect("tr") |> List.map createSighting
+
+    0 // return an integer exit code
